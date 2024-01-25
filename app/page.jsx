471770a8d98fb167698a248df2877f8fa8b33d26
pivanrios <pipr96@gmail.com>
2024-01-24 18:25:06 -0800

    add components

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Feed from "@components/Feed"
 import Image  from "next/image"
 import Subscription from "@components/Subscription"
+import Footer from "@components/Footer"
 
 function Home() {
   return (
+    <>
     <section className="w-full flex-center flex-col">
         <Image
               src="/icons/logo.png"
@@ -11,17 +13,18 @@ function Home() {
               height={400}
               />
         <h1 className="head_text text-center">
-          Lunch
+          Lunche
           <br />
           <br className="max-md:hidden"/>
           <span className="orange_gradient text-center">Meal Prep</span>
         </h1>
         <p className="desc text-center">
-          Order homemade food from your cooworkers and 
-          and get rewards for subscibing
+          Order homemade food from your cooworkers, save money and get rewards by <a href="/">subscibing</a>.
         </p>
-        <Subscription/>
     </section>
+    <Subscription/>
+    <Footer/>
+    </>
   )
 }
 
