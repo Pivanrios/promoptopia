f83d49df5a9d470cf4acaf2e838aafead00a7a36
pivanrios <pipr96@gmail.com>
2024-01-24 18:25:38 -0800

    create table

--- components/Subscription.jsx
+++ components/Subscription.jsx
@@ -1,29 +1,41 @@
-
+"use client"
 
 const Subscription = () => {
   return (
-    <section>
-        <article>
-            <h4>Subcripciones</h4>
-                <table>
-                    <tr> 
+    <section className="w-full flex-center flex-col ">
+        <article className="w-full flex-center flex-col p-10">
+                    <table 
+                        className="w-full">
+                        <thead>
+                            <tr className="text-center w-full">
+                            <th colspan="2"
+                                className=" border-b-2">Supscripciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr 
+                        className="text-center "> 
                         <td>2 Platillos</td>
                         <td>$0.25</td>
                         <td>x platillo</td>
                     </tr>
-                    <tr> 
+                    <tr
+                        className="text-center border-2 w-full"> 
                         <td>3 Platillos</td>
                         <td>$0.50</td>
                         <td>x platillo</td>
                     </tr>
-                    <tr> 
+                    <tr
+                        className="text-center border-2"> 
                         <td>4 Platillos</td>
                         <td>$0.75</td>
                         <td>x platillo</td>
                     </tr>
+                        </tbody>
                 </table>
+
         </article>
-        <article>
+        <article className="w-full flex-center flex-col p-100">
             <h4>Combo</h4>
             <ol>
                 <li>Desayuno.......</li>
