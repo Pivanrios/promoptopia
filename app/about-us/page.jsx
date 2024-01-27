ce9cc5ea835311d7f55e764e7126f2b9f14a05eb
pivanrios <pipr96@gmail.com>
2024-01-27 10:21:48 -0800

    add content

--- app/about-us/page.jsx
+++ app/about-us/page.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import Image from "next/image"
 
 const AboutUs = () => {
   return (
@@ -7,6 +7,34 @@ const AboutUs = () => {
            className='head_text text-center' >
             About us
         </h3>
+        {/*-------------------------Desktop version ------------------------ */}
+        <article className=" flex flex-row px-2 py-7" >
+          <Image  
+                src={"/icons/peter.png"}
+                width={300}
+                height={300}/>
+          <div className="flex flex-col p-3 gap-3">
+            <h5 className="head_text text-center p-4">Community</h5>
+              
+            <p>We believe that home made food is more tasty and healthy than frozen food in our workplace.
+              By overdoing lunch you can share your food with your coworkers and..
+             <a href="/about-us">here!</a> </p>
+          </div>
+          
+        </article>
+        <article className=" flex flex-row px-2 py-7" >
+          <Image  
+                src={"/icons/peter.png"}
+                width={300}
+                height={300}/>
+          <div className="flex flex-col p-3 gap-3">
+            <h5 className="head_text text-center p-4">Whatsapp</h5>
+              
+            <p>Join our Whatsapp group and answer the poll to order for the food of the day. <a href="/about-us">here!</a> </p>
+          </div>
+          
+        </article>
+        {/*-----------------Mobile version------------------------------------- */}
     </section>
   )
 }
