cba03d69a16c2a1a01e5999dde81ced1f11ee88a
pivanrios <pipr96@gmail.com>
2024-01-26 22:22:08 -0800

    add api endpoint wiht menu array

--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -1,11 +1,39 @@
+"use client"
+import { useEffect, useState } from "react"
+
 
 const menu = () => {
+  const [dishes, setDishes] = useState([])
+
+
+  useEffect(()=>{
+    console.log("inside useEffect function")
+    //fetch data from the api
+    const setData = async () => {
+      console.log("setting data..")
+      const res = await fetch("http://localhost:3000/api/menu");
+      const menu = await res.json();
+      setDishes(menu);
+      console.log("Menu:", menu);
+    }
+    setData();//invoke function
+
+  },[])
+
+
   return (
     <section>
         <h2 
         className="head_text text-center">
             menu
         </h2>
+        <div>
+            {dishes.map((dish)=>(
+              <p key={dish.id}>{dish.name} precio: ${dish.price}.00</p>
+            ))}
+
+
+        </div>
 
     </section>
   )
--- app/page.jsx
+++ app/page.jsx
@@ -8,18 +8,13 @@ function Home() {
   return (
     <>
     <section className="w-full flex-center flex-col">
-        <Slider/>
-        <Image
-              src="/icons/rifa.jpeg"
-              width={300}
-              height={400}
-              />
         <h1 className="head_text text-center">
           Lunche
           <br />
           <br className="max-md:hidden"/>
           <span className="orange_gradient text-center">Meal Prep</span>
         </h1>
+        <Slider/>
         <p className="desc text-center">
           Order homemade food from your cooworkers, save money and get rewards by <a href="/">subscibing</a>.
         </p>
