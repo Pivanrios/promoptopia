dc7e0b5b8ed54517c371f5b8a2dc389cc87c751e
pivanrios <pipr96@gmail.com>
2024-01-29 17:27:50 -0800

    add ingredients and change style

--- app/menu/[dishId]/page.jsx
+++ app/menu/[dishId]/page.jsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react"
 const Dish = ({params}) => {
 
   const [dish, setDish] = useState({});
+  const [ingredients, setIngredients] = useState([]);
 
     useEffect(()=>{
         //Set data
@@ -17,6 +18,7 @@ const Dish = ({params}) => {
             const  result = await res.json(); //convert response to json
             console.log("result:",result);
             setDish(result);
+            setIngredients(result.ingredientes);
             
 
            } catch (error) {
@@ -29,17 +31,19 @@ const Dish = ({params}) => {
 
   return (
     <section>
-      <article className="border-2 p-3">
+      <article className="border-2 p-3 flex flex-row gap-2">
         <Image
             src={dish.imageUrl}
             width={50}
             height={50}
             alt={dish.name}/>
-        <h6>{dish.name}</h6>
-        <p>{dish.category}</p>
-        <p>{dish.desc}</p>
-        <p>{}</p>
-        <p>price: ${dish.price}.00</p>
+        <div>
+          <h6 className="font-bold ">{dish.name}</h6>
+          <p className="font-thin">{dish.category}</p>
+          <p className="">{dish.desc}</p>
+          {ingredients.map((e)=>(<p>{e}</p>))}
+          <p className="">price: ${dish.price}.00</p>
+        </div>
       </article>
 
     </section>
--- components/Product.jsx
+++ components/Product.jsx
@@ -5,18 +5,20 @@ const Product = ({id, name, price, desc, imageUrl}) => {
   return (
     <Link href={`/menu/${name}`}>
     <div 
-            className="w-full border-2 py-2 px-5  "
+            className="w-full border-2 py-2 px-5 flex flex-row gap-2 "
             >
                 
         <Image
                 src={imageUrl}
                 width={40}
-                height={40}
+                height={70}
                 alt={name}/>
-        <h4>{name}</h4>
-        
-        <p>{desc}</p>
-        <p>Price: ${price}.00</p>
+        <div>
+          <h4>{name}</h4>
+          
+          <p>{desc}</p>
+          <p>Price: ${price}.00</p>
+        </div>
         
     </div>
     </Link>
