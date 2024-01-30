cb15314fe9543e7406ee8ce4da4e67fb9a121990
pivanrios <pipr96@gmail.com>
2024-01-29 23:38:04 -0800

    change name and apdate object

--- utils/functions.js
+++ utils/functions.js
@@ -2,20 +2,25 @@
 //crear menu
 const menu = [{id:1, name:"lasagna",price:8}, {id:2, name:"hotcakes", price:4}, {id:3, name:"rajas",price:3}];
 //nuevo conjunto
-const lunes = [{dia:1, name:'lasagna'}, {dia:3, name:"rajas"}];
+const semana = [{dia:1, platillos:['lasagna','hotcakes']}, {dia:3, platillos:["mole", "burritos"]}];
 
 const checkForDishes = (x, y)=>{
     //const interseccion
     const res = [];
     x.forEach(e=>{//search in every element fo the menu
-        y.forEach(j =>{// loop tru every element on day
-            if(e.name == j.name){
-                res.push(e);
-            }
+        y.forEach(j =>{// loop tru every element on platillos de dia de la semana semana
+            console.log("j:",j.platillos);
+            j.platillos.forEach(dish =>{
+                console.log("dish",dish);
+                if (dish == e.name){
+                    res.push(e);
+                }
+            })
         })
     } );
     return res;
 }
-console.log("invoking funcion",checkForDishes(menu, lunes));
+console.log("invoking funcion",checkForDishes(menu, semana));
 
-export default checkForDishes();
+
+export default checkForDishes;
